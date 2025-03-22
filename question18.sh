5cb0d3615c51d9f1980f06fe7062912fc79b57fd
prajwal801 <123920656+prajwal801@users.noreply.github.com>
2025-03-22 15:13:23 +1030

    Add files via upload

---
+++ question18.sh
@@ -0,0 +1,16 @@
+git fetch origin
+git checkout main
+for branch in $(git branch -r | grep 'origin/ready'); do
+  local_branch=${branch#origin/}
+  git checkout -b $local_branch $branch
+  git checkout main
+  git merge $local_branch
+  git branch -d $local_branch
+done
+
+for branch in $(git branch -r | grep 'origin/update'); do
+  local_branch=${branch#origin/}
+  git checkout -b $local_branch $branch
+  git merge main
+done
+git checkout main
